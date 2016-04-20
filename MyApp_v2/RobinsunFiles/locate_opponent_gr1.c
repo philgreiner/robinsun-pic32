5e05903bdce6acb4ba20299afa0d4721813220d0
philgreiner <phil_greiner@hotmail.com>
2016-04-20 10:43:52 +0200

    Changed ignore cabins

--- MyApp_v2/RobinsunFiles/locate_opponent_gr1.c
+++ MyApp_v2/RobinsunFiles/locate_opponent_gr1.c
@@ -213,7 +213,7 @@ int isMeasureRelevant(CtrlStruct *cvs, double xFound, double yFound)
 	 	return 0;
      else if((cvs->state->position[1]/fabs(cvs->state->position[1]) != yFound/fabs(yFound)) && xFound > -0.3 && xFound < 0.4) // don't look through the central wall
         return 0;
-     else if(fabs(yFound)<1.35 && fabs(yFound) > 0.8 && xFound < -0.8)
+     else if(fabs(yFound)<1.35 && fabs(yFound) > 0.8 && xFound < -0.85) // ignore cabins
         return 0;
 	 else // the obstacle detected is an opponent
 	 	return 1;
